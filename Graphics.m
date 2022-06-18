e8e033b55f5c4c4b7ecba80104337decb4e31cfe
FranGarcia94 <107102754+FranGarcia94@users.noreply.github.com>
2022-06-18 04:31:21 +0200

    Add files via upload

---
+++ Graphics.m
@@ -0,0 +1,26 @@
+% Gráficas
+
+% plot(vec_coste(1,:),'b');hold on;plot(vec_coste(2,:),'r');plot(vec_coste(3,:),'y');plot(vec_coste(4,:),'g');plot(vec_coste(5,:),'m');plot(vec_coste(6,:),'k');grid on
+% 
+% figure
+% 
+% plot(vec_excesos(1,:),'r');hold on;plot(vec_precio(1,:),'y');plot(vec_coste(1,:),'b');plot(find(vec_coste(1,:)==min(vec_coste(1,:))),min(vec_coste(1,:)),'o');yline(min(vec_coste(1,:)),'--')
+% figure
+% plot(vec_excesos(2,:),'r');hold on;plot(vec_precio(2,:),'y');plot(vec_coste(2,:),'b');plot(find(vec_coste(2,:)==min(vec_coste(2,:))),min(vec_coste(2,:)),'o');yline(min(vec_coste(2,:)),'--')
+% figure
+% plot(vec_excesos(3,:),'r');hold on;plot(vec_precio(3,:),'y');plot(vec_coste(3,:),'b');plot(find(vec_coste(3,:)==min(vec_coste(3,:))),min(vec_coste(3,:)),'o');yline(min(vec_coste(3,:)),'--')
+% figure
+% plot(vec_excesos(4,:),'r');hold on;plot(vec_precio(4,:),'y');plot(vec_coste(4,:),'b');plot(find(vec_coste(4,:)==min(vec_coste(4,:))),min(vec_coste(4,:)),'o');yline(min(vec_coste(4,:)),'--')
+% figure
+% plot(vec_excesos(5,:),'r');hold on;plot(vec_precio(5,:),'y');plot(vec_coste(5,:),'b');plot(find(vec_coste(5,:)==min(vec_coste(5,:))),min(vec_coste(5,:)),'o');yline(min(vec_coste(5,:)),'--')
+% figure
+% plot(vec_excesos(6,:),'r');hold on;plot(vec_precio(6,:),'y');plot(vec_coste(6,:),'b');plot(find(vec_coste(6,:)==min(vec_coste(6,:))),min(vec_coste(6,:)),'o');yline(min(vec_coste(6,:)),'--')
+
+% Gráficas de como influye los excesos y precios de cada periodo
+% individualente, marcando sus mínimos, es decir, cual sería la potencia
+% contratada ideal teniendo en cuenta cada periodo por separado
+
+%% Potencias óptimas individuales
+for i=1:6
+    minimo(i)=find(vec_coste(i,:)==min(vec_coste(i,:)));
+end
---
+++ Set_Periods.m
@@ -0,0 +1,49 @@
+%% Establecer Periodos (discrimina Fines de Semana)
+periodo=zeros(size(t2,1),1);
+
+i=1;
+while(i<=size(t2,1))  
+    switch t2.Fecha(i).Month
+        case {1,2,7,12}
+            switch t2.Horas(i)
+                case {0,1,2,3,4,5,6,7,24} % Siempre igual
+                    periodo(i)=6;
+                case {8,14,15,16,17,22,23}
+                    periodo(i)=2;
+                case {9,10,11,12,13,18,19,20,21}
+                    periodo(i)=1;
+            end
+        case {3,11}
+            switch t2.Horas(i)
+                case {0,1,2,3,4,5,6,7,24} % Siempre igual
+                    periodo(i)=6;
+                case {8,14,15,16,17,22,23}
+                    periodo(i)=3;
+                case {9,10,11,12,13,18,19,20,21}
+                    periodo(i)=2;
+            end
+        case {4,5,10}
+            switch t2.Horas(i)
+                case {0,1,2,3,4,5,6,7,24} % Siempre igual
+                    periodo(i)=6;
+                case {8,14,15,16,17,22,23}
+                    periodo(i)=5;
+                case {9,10,11,12,13,18,19,20,21}
+                    periodo(i)=4;
+            end
+        case {6,8,9}
+            switch t2.Horas(i)
+                case {0,1,2,3,4,5,6,7,24} % Siempre igual
+                    periodo(i)=6;
+                case {8,14,15,16,17,22,23}
+                    periodo(i)=4;
+                case {9,10,11,12,13,18,19,20,21}
+                    periodo(i)=3;
+            end
+    end
+    if (isweekend(t2.Fecha(i))==1)
+        periodo(i)=6;
+    end
+
+    i=i+1;
+end
